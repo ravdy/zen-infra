641378b6b957fdcc9ee6d05dfa7119a2357a2c21
oolusoga <olaolusoga2013@gmail.com>
2026-04-18 11:25:50 +0100

    fix: enable force_delete for ECR repositories to allow terraform destroy

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -2,6 +2,7 @@ resource "aws_ecr_repository" "main" {
   for_each = toset(var.repositories)
 
   name                 = each.value
+  force_delete         = true
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
