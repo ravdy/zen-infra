d6f15ac69fd9fa865591ffa1622f8a76287619a9
viraj1004 <lukkaviraj@gmail.com>
2026-04-26 10:23:02 +0000

    removed extra z

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-284934320341"
+    bucket       = "zen-pharma-terraform-state-viraj9676"
     key          = "envs/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
