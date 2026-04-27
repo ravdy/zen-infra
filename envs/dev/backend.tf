30d23265bc7aff825c1643c358fd8a720ee374f2
viraj1004 <lukkaviraj@gmail.com>
2026-04-27 08:03:14 +0000

    20260427

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "zen-pharma-terraform-state-viraj9676"
-    key     = "envs/dev/terraform.tfstate"
+    key     = "envs/dev/.terraform/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true # S3 native locking — requires Terraform 1.10+, no DynamoDB needed
