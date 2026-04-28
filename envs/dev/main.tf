521fc27df5d3590e165c5fbf0bcd21940e2fc4d9
Niranjan Gowdru Suresh <nsuresh@adobe.com>
2026-04-28 09:22:32 +0530

    Node start

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -19,7 +19,7 @@ module "eks" {
   cluster_version    = "1.33"
   subnet_ids         = module.vpc.private_eks_subnet_ids
   node_instance_type = "t3.small"
-  desired_capacity   = 3
+  desired_capacity   = 3 #start node
   min_size           = 1
   max_size           = 4
 }
